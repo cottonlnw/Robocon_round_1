78aba70c27bd4b78c719f13745aa886a2ef5ea29
cottonlnw <thanakorn.soiso@mail.kmutt.ac.th>
2025-05-20 04:16:27 +0700

    Update Rack.ino

--- In progress/Rack.ino
+++ In progress/Rack.ino
@@ -1,32 +1,37 @@
-bool rackUpReached = false;
+// bool rackUpReached = false;
 
-void auto_racking(int uk){
-  bool upState = digitalRead(limitUp);       // HIGH = ไม่ชน / LOW = ชน
-  bool downState = digitalRead(limitDown);
+// void auto_racking(int uk){
+//   bool upState = digitalRead(limitUp);       // HIGH = ไม่ชน / LOW = ชน
+//   bool downState = digitalRead(limitDown);
+//   Serial.print(uk);
 
-  if (uk == 1 && !rackUpReached && upState == 0) {
-    digitalWrite(motorRACK1,HIGH);
-    digitalWrite(motorRACK2,LOW);
-    analogWrite(motorPWM,120);
-  }
-  else if (uk == 1 && upState == 1) {
-    rackUpReached = true;
-    digitalWrite(motorRACK1, HIGH);
-    digitalWrite(motorRACK2, HIGH);
-    analogWrite(motorPWM, 0);
-  }
-  else if (uk == 0 && downState == 0) {
-    rackUpReached = false;
-    digitalWrite(motorRACK1,LOW);
-    digitalWrite(motorRACK2,HIGH);
-    analogWrite(motorPWM, 30);
-  }
-  else if (uk == 0 && downState == 1) {
-    digitalWrite(motorRACK1, HIGH);
-    digitalWrite(motorRACK2, HIGH);
-    analogWrite(motorPWM, 0);
-  }
-}
+//   if (uk == 1 && !rackUpReached && upState == 0) {
+//     digitalWrite(motorRACK1,HIGH);
+//     digitalWrite(motorRACK2,LOW);
+//     analogWrite(motorPWM,120);
+//     Serial.print("  1");
+//   }
+//   else if (uk == 1 && upState == 1) {
+//     rackUpReached = true;
+//     digitalWrite(motorRACK1, LOW);
+//     digitalWrite(motorRACK2, LOW);
+//     analogWrite(motorPWM, HIGH);
+//     Serial.print("2");
+//   }
+//   else if (uk == 0 && downState == 0) {
+//     rackUpReached = false;
+//     digitalWrite(motorRACK1,LOW);
+//     digitalWrite(motorRACK2,HIGH);
+//     analogWrite(motorPWM, 20);
+//     Serial.print("3");
+//   }
+//   else if (uk == 0 && downState == 1) {
+//     digitalWrite(motorRACK1, HIGH);
+//     digitalWrite(motorRACK2, HIGH);
+//     analogWrite(motorPWM, 0);
+//     Serial.print("4");
+//   }
+// }
 void manual_rack(int mu, int md){
   bool upState = digitalRead(limitUp);       // HIGH = ไม่ชน / LOW = ชน
   bool downState = digitalRead(limitDown);
@@ -42,23 +47,28 @@ void manual_rack(int mu, int md){
     digitalWrite(motorRACK1, HIGH);
     digitalWrite(motorRACK2, HIGH);
     analogWrite(motorPWM, 0);
+    Serial.print("5");
   }else if(md ==1 and downState==1){
     digitalWrite(motorRACK1, HIGH);
     digitalWrite(motorRACK2, HIGH);
     analogWrite(motorPWM, 0);
+    Serial.print("6");
   }else if(mu == 1){
     digitalWrite(motorRACK1,HIGH);
     digitalWrite(motorRACK2,LOW);
-    analogWrite(motorPWM,100);
+    analogWrite(motorPWM,180);
+    Serial.print("7");
   }else if(md ==1){
     digitalWrite(motorRACK1,LOW);
     digitalWrite(motorRACK2,HIGH);
-    analogWrite(motorPWM, 20);
+    analogWrite(motorPWM, 40);
+    Serial.print("8");
   }
   else {
   digitalWrite(motorRACK1, HIGH);
   digitalWrite(motorRACK2, HIGH);
   analogWrite(motorPWM, 0);
+  Serial.print("9");
 }
 
 }
